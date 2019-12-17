b8e6f6f0f6705bfd5c5943befb235ec61c60a1d4
huadiweilaoqy <1315606422@qq.com>
2019-12-17 17:05:23 +0800

    LIS3DHTR

--- examples/LIS3DHTR_SPI/LIS3DHTR_SPI.ino
+++ examples/LIS3DHTR_SPI/LIS3DHTR_SPI.ino
@@ -1,26 +1,10 @@
 #include "LIS3DHTR.h"
-
-// if you use the software I2C to drive the sensor, you can uncommnet the define SOFTWAREWIRE which in LIS3DHTR.h.
-#ifdef SOFTWAREWIRE
-#include <SoftwareWire.h>
-SoftwareWire myWire(3, 2);
-LIS3DHTR<SoftwareWire> LIS(0);
-#define WIRE myWire
-#else
-#include <Wire.h>
-//  LIS3DHTR<TwoWire> LIS(0);//I2C
 LIS3DHTR<TwoWire> LIS(1); //SPI
-#define WIRE Wire
-#endif
-
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial)
-  {
-  };
-  // LIS.begin(WIRE, 0x19); //IIC address
-  LIS.begin(10); //SPI SS
+  while (!Serial);
+  LIS.begin(10); //SPI SS/CS
   //  LIS.openTemp();
   //  LIS.closeTemp();//default
   delay(100);
@@ -42,18 +26,18 @@ void loop()
   if (!LIS)
   {
     Serial.println("LIS3DHTR didn't connect.");
-    while (1)
-      ;
+    while(1);
     return;
   }
+  //3 Axis
   Serial.print("x:");Serial.print(LIS.getAccelerationX());Serial.print("  ");
   Serial.print("y:");Serial.print(LIS.getAccelerationY());Serial.print("  ");
   Serial.print("z:");Serial.println(LIS.getAccelerationZ());
-
-  //  Serial.print("adc1:"); Serial.println(LIS.readbitADC1());
-  //  Serial.print("adc2:"); Serial.println(LIS.readbitADC2());
-  //  Serial.print("adc3:"); Serial.println(LIS.readbitADC3());
-
-  //  Serial.print("temp:"); Serial.println(LIS.getTemperature());
+  //ADC
+//    Serial.print("adc1:"); Serial.println(LIS.readbitADC1());
+//    Serial.print("adc2:"); Serial.println(LIS.readbitADC2());
+//    Serial.print("adc3:"); Serial.println(LIS.readbitADC3());
+  //temperature
+//    Serial.print("temp:"); Serial.println(LIS.getTemperature());
   delay(500);
 }
